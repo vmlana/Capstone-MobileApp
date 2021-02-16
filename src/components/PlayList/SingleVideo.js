ee6c7a16bbdcc5f05826e7e92d04f7b4af4b689c
Ken Tauchi <kentauchica@gmail.com>
2021-02-16 01:23:20 -0800

    add video player

--- src/components/PlayList/SingleVideo.js
+++ src/components/PlayList/SingleVideo.js
@@ -1,12 +1,34 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { Video } from "expo-av";
+import { View, Text, TouchableHighlight, Dimensions } from "react-native";
+
+let video =
+  "https://static.videezy.com/system/resources/previews/000/034/463/original/C0002_6_copy.mp4";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const SingleVideo = () => {
-    return (
-        <View>
-            <Text>Single Video</Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Video
+        source={{
+          uri: "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+        }}
+        rate={1.0}
+        volume={1.0}
+        isMuted={false}
+        useNativeControls
+        resizeMode="stretch"
+        shouldPlay
+        isLooping
+        style={{ width: windowWidth, height: 250 }}
+        onPlaybackStatusUpdate={(playbackStatus) =>
+          playbackStatus.didJustFinish ? console.log("done") : null
+        }
+      />
+    </View>
+  );
+};
 
-export default SingleVideo
+export default SingleVideo;
---
+++ src/screens/SingleVideoScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+// component ===============
+import SingleVideo from "../components/PlayList/SingleVideo";
+
+const SingleVideoScreen = () => {
+  return (
+    <View>
+      <SingleVideo />
+    </View>
+  );
+};
+
+export default SingleVideoScreen;
+
+const styles = StyleSheet.create({});
