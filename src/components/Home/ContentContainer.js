bee5871f1176eee431ea13faa415f375a3c45400
H J Vasoya <65635239+hjvasoya009@users.noreply.github.com>
2021-02-19 17:03:24 -0800

    Trainer API Fetch

--- src/components/Home/ContentContainer.js
+++ src/components/Home/ContentContainer.js
@@ -3,81 +3,85 @@ import styled from "styled-components";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import {
-  Container,
-  Title,
-  Button,
-  Left,
-  Right,
-  Body,
-  Icon,
-  Text,
-  Thumbnail,
+	Container,
+	Title,
+	Button,
+	Left,
+	Right,
+	Body,
+	Icon,
+	Text,
+	Thumbnail,
 } from "native-base";
 import { View, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 import { videoData } from "../../demoData";
 
 const ContentContainer = ({ navigation, result, type }) => {
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => navigation.navigate("SingleVideo", { data: videoData })}
-      >
-        <ThumbNail type={type} source={{ uri: result.thumbNail }} />
-      </TouchableOpacity>
-      {type === "cat" || type === "movie" ? <Text>{result.title}</Text> : null}
+	return (
+		<View style={styles.container}>
+			<TouchableOpacity
+				onPress={() => navigation.navigate("SingleVideo", { data: videoData })}
+			>
+				<ThumbNail type={type} source={{ uri: result.thumbNail }} />
+			</TouchableOpacity>
+			{type === "cat" || type === "movie" ? <Text>{result.title}</Text> : null}
 
-      {type === "movie" ? (
-        <View style={styles.trainerContainer}>
-          <Thumbnail
-            style={styles.trainerThumb}
-            small
-            source={{
-              uri: result.trainerPic,
-            }}
-          />
+			{type === "movie" ? (
+				<TouchableOpacity
+					onPress={() => navigation.navigate("TrainerDetails", { name: result.trainer, id: 2 })}
+				>
+					<View style={styles.trainerContainer}>
+						<Thumbnail
+							style={styles.trainerThumb}
+							small
+							source={{
+								uri: result.trainerPic,
+							}}
+						/>
 
-          <Text style={{ fontSize: 12 }}>{result.trainer}</Text>
-        </View>
-      ) : type === "playlist" ? (
-        <View style={styles.playListContainer}>
-          <Text>{result.title}</Text>
-          <View style={{ flexDirection: "row" }}>
-            <MaterialCommunityIcons
-              name="checkbox-multiple-blank-outline"
-              size={15}
-              color="black"
-              style={{ marginRight: 5, alignItems: "center" }}
-            />
-            <Text>{result.movieLists.length}</Text>
-          </View>
-        </View>
-      ) : null}
-    </View>
-  );
+						<Text style={{ fontSize: 12 }}>{result.trainer}</Text>
+					</View>
+				</TouchableOpacity>
+			) : type === "playlist" ? (
+				<View style={styles.playListContainer}>
+					<Text>{result.title}</Text>
+					<View style={{ flexDirection: "row" }}>
+						<MaterialCommunityIcons
+							name="checkbox-multiple-blank-outline"
+							size={15}
+							color="black"
+							style={{ marginRight: 5, alignItems: "center" }}
+						/>
+						<Text>{result.movieLists.length}</Text>
+					</View>
+				</View>
+			) : null}
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    marginTop: 10,
-    marginBottom: 10,
-    marginRight: 20,
-  },
-  trainerContainer: {
-    flexDirection: "row",
-    marginTop: 7,
-    alignItems: "center",
-  },
-  trainerThumb: {
-    width: 24,
-    height: 24,
-    marginRight: 10,
-  },
-  playListContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
+	container: {
+		display: "flex",
+		marginTop: 10,
+		marginBottom: 10,
+		marginRight: 20,
+	},
+	trainerContainer: {
+		flexDirection: "row",
+		marginTop: 7,
+		alignItems: "center",
+	},
+	trainerThumb: {
+		width: 24,
+		height: 24,
+		marginRight: 10,
+	},
+	playListContainer: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+	},
 });
 
 const ThumbNail = styled.Image`
