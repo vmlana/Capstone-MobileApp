b6e1b46de8d2b77dbede949e92e9e7441413adf2
TOMO-YOSHI <lucky.paddy10@gmail.com>
2021-02-21 06:03:32 -0800

    CAP-144 Create PlayListScreen

--- node_modules/react-native-media-controls/src/constants/playerStates.ts
+++
@@ -1,7 +0,0 @@
-enum PLAYER_STATES {
-  PLAYING = 0,
-  PAUSED = 1,
-  ENDED = 2,
-}
-
-export { PLAYER_STATES };
--- src/screens/PlayListScreen.js
+++ src/screens/PlayListScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, FlatList, StyleSheet, Image } from "react-native";
 
 const PlayListScreen = ({ navigation }) => {
   // data and playList contain info of single videos and playlist respectively passed to the parameter from the ContentContainer component.
@@ -16,9 +16,19 @@ const PlayListScreen = ({ navigation }) => {
     setPlayList(playList);
   }, []);
 
+  if (!data || !playList) {
+    return (
+        <View style={styles.centered}>
+            <Text>No lessons found.</Text>
+        </View>
+    );
+  }
+
   return (
-    <View>
-      {singleVideos
+    <View
+      style={styles.container}
+    >
+      {/* {singleVideos
         ? singleVideos.map((singleVideo, index) => (
             <View key={index}>
               <Text>{singleVideo.lessonName}</Text>
@@ -35,9 +45,144 @@ const PlayListScreen = ({ navigation }) => {
               </TouchableOpacity>
             </View>
           ))
-        : null}
+        : null} */}
+        <FlatList
+          data={playListData.lessons}
+          keyExtractor={(item)=> item.lessonId.toString()}
+          renderItem={({item, index}) => {
+            return (
+            <View>
+              {
+                index % 2 === 0
+                ?
+                <TouchableOpacity
+                  onPress={() =>
+                    navigation.navigate("SingleVideo", {
+                      //   passing the parameter to the single video screen
+                      videoData: item,
+                      playListData,
+                    })
+                  }
+                  style={styles.lessonContainer}
+                >
+                  <Image
+                    style={styles.image}
+                    // source={{ uri: item.imageFile }}
+                    source={{ uri: "https://pivotcare-s3.s3-us-west-2.amazonaws.com/" + item.imageFile }}
+                  />
+                  <View style={styles.nameAndDesView}>
+                    <Text 
+                      style={styles.lessonName}
+                    >
+                      {item.lessonName}
+                    </Text>
+                    <View>
+                      <Text 
+                        style={styles.lessonDescription}
+                      >
+                        {item.lessonDescription}
+                      </Text>
+                    </View>
+                    <Text style={styles.duration}>Duration: 20 mins</Text>
+                  </View>
+                </TouchableOpacity>
+              :
+              <TouchableOpacity
+                  onPress={() =>
+                    navigation.navigate("SingleVideo", {
+                      //   passing the parameter to the single video screen
+                      videoData: item,
+                      playListData,
+                    })
+                  }
+                  style={styles.lessonContainer}
+                >
+                  <View style={styles.nameAndDesViewLeft}>
+                    <Text 
+                      style={styles.lessonName}
+                    >
+                      {item.lessonName}
+                    </Text>
+                    <View>
+                      <Text 
+                        style={styles.lessonDescription}
+                      >
+                        {item.lessonDescription}
+                      </Text>
+                    </View>
+                    <Text style={styles.duration}>Duration: 20 mins</Text>
+                  </View>
+                  <Image
+                    style={styles.image}
+                    // source={{ uri: item.imageFile }}
+                    source={{ uri: "https://pivotcare-s3.s3-us-west-2.amazonaws.com/" + item.imageFile }}
+                  />
+                </TouchableOpacity>
+              }
+            </View>
+            )}
+          }
+        />
     </View>
   );
 };
+
+PlayListScreen.navigationOptions = (navData) => {
+  const playList = navData.navigation.getParam("playListData");
+  return {
+      headerTitle: playList.playlistName,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      padding: 33,
+  },
+  centered: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+  },
+  lessonContainer: {
+    flexDirection:'row',
+    marginBottom: 23
+  },
+  image: {
+    width: 135,
+    height: 142,
+    borderRadius: 5
+  },
+  nameAndDesView: {
+    paddingTop: 26,
+    paddingLeft: 26,
+    paddingBottom: 26,
+    flex: 1,
+    flexDirection: "column",
+  },
+  nameAndDesViewLeft: {
+    paddingTop: 26,
+    paddingRight: 26,
+    paddingBottom: 26,
+    flex: 1,
+    flexDirection: "column",
+  },
+  lessonName: {
+    fontSize: 19,
+    marginBottom: 11,
+    color: "#707070"
+  },
+  lessonDescription: {
+    fontSize: 13,
+    fontWeight: "300",
+    marginBottom: 11,
+    color: "#707070"
+  },
+  duration: {
+    fontSize: 13,
+    fontWeight: "300",
+    color: "#707070"
+  }
+});
 
 export default PlayListScreen;
